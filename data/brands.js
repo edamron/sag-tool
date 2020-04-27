9afa62638135c0c638d3baffacfbd058a17011f7
Earl Damron <e@earldamron.com>
2020-04-27 16:57:09 -0700

    got pickers and data filtering logic in place, and moved spec-api into its own folder

--- data/brands.js
+++ data/brands.js
@@ -1,5 +1,4 @@
 export const Brands = [
-	'Harley Davidson',
 	'Yamaha',
 	'Honda',
 	'Ducati',
@@ -11,35 +10,62 @@ export const Brands = [
 	'KTM',
 ];
 
+/*
+get distinct values for make in 2014
+- db["suspension-specs"].distinct( "make", { year: 2014 } )
+which returns [ "Aprilia", "BMW", "Ducati", "Honda", "KTM", "Suzuki" ]
+
+get all the 2014 Decati models
+- db["suspension-specs"].find({ year: 2014, make: 'Ducati' }, { model: 1 })
+which returns { "_id" : ObjectId("5ea730dcbc29de287aad149a"), "model" : "Scrambler" }
+
+a regular find, returning only make
+db["suspension-specs"].find({ year: 2014 }, { make: 1 })
+
+the rows I added to the database:
+
+db["suspension-specs"].insertMany([
+	{ front: 0, rear: 0, year: 2016, make: 'Yamaha', model: 'MT-09' },
+	{ front: 0, rear: 0, year: 2015, make: 'Yamaha', model: 'Virago XV400' },
+	{ front: 0, rear: 0, year: 2018, make: 'Yamaha', model: 'Venture' },
+	{ front: 0, rear: 0, year: 2014, make: 'Honda', model: 'Transalp' },
+	{ front: 0, rear: 0, year: 2017, make: 'Honda', model: 'CBR1000RR' },
+	{ front: 0, rear: 0, year: 2019, make: 'Honda', model: 'RC212V' },
+	{ front: 0, rear: 0, year: 2018, make: 'Honda', model: 'CB1300' },
+	{ front: 0, rear: 0, year: 2020, make: 'Ducati', model: 'Monster 696' },
+	{ front: 0, rear: 0, year: 2013, make: 'Ducati', model: 'Panigale V4' },
+	{ front: 0, rear: 0, year: 2014, make: 'Ducati', model: 'Scrambler' },
+	{ front: 0, rear: 0, year: 2015, make: 'Ducati', model: '1198' },
+	{ front: 0, rear: 0, year: 2016, make: 'Kawasaki', model: 'Vulcan' },
+	{ front: 0, rear: 0, year: 2017, make: 'Kawasaki', model: '800 Classic' },
+	{ front: 0, rear: 0, year: 2018, make: 'Kawasaki', model: 'KLR650' },
+	{ front: 0, rear: 0, year: 2019, make: 'Kawasaki', model: 'Versys' },
+	{ front: 0, rear: 0, year: 2020, make: 'BMW', model: 'R 1250 RS' },
+	{ front: 0, rear: 0, year: 2014, make: 'BMW', model: 'K 1600 GT' },
+	{ front: 0, rear: 0, year: 2014, make: 'BMW', model: 'F800GT' },
+	{ front: 0, rear: 0, year: 2014, make: 'BMW', model: 'HP4 Race' },
+	{ front: 0, rear: 0, year: 2014, make: 'Suzuki', model: 'GSX-R1000' },
+	{ front: 0, rear: 0, year: 2016, make: 'Suzuki', model: 'Boulevard C90' },
+	{ front: 0, rear: 0, year: 2018, make: 'Suzuki', model: 'DR650' },
+	{ front: 0, rear: 0, year: 2017, make: 'Suzuki', model: 'SV650' },
+	{ front: 0, rear: 0, year: 2018, make: 'Aprilia', model: 'Tuono 100R' },
+	{ front: 0, rear: 0, year: 2019, make: 'Aprilia', model: 'RSV4' },
+	{ front: 0, rear: 0, year: 2020, make: 'Aprilia', model: 'SL 750' },
+    { front: 0, rear: 0, year: 2014, make: 'Aprilia', model: 'SR50' }
+])
+db["suspension-specs"].insertMany([
+    { front: 0, rear: 0, year: 2015, make: 'Triumph', model: 'Street Triple RS' },
+	{ front: 0, rear: 0, year: 2016, make: 'Triumph', model: 'Speed Triple' },
+	{ front: 0, rear: 0, year: 2017, make: 'Triumph', model: 'Thruxton' },
+	{ front: 0, rear: 0, year: 2018, make: 'Triumph', model: 'Bonneville' },
+	{ front: 0, rear: 0, year: 2019, make: 'KTM', model: '1290 Super Duke' },
+	{ front: 0, rear: 0, year: 2020, make: 'KTM', model: '690 Enduro' },
+	{ front: 0, rear: 0, year: 2014, make: 'KTM', model: '390 Adventure' },
+	{ front: 0, rear: 0, year: 2015, make: 'KTM', model: '890 Duke R' }    
+])
+*/
+
 export const Brands2 = [
-	{
-		front: 0,
-		rear: 0,
-		year: 2019,
-		make: 'Harley Davidson',
-		model: 'FLHT Electra Glide',
-	},
-	{
-		front: 100,
-		rear: 70,
-		year: 2018,
-		make: 'Harley Davidson',
-		model: 'VRSCB V-Rod',
-	},
-	{
-		front: 0,
-		rear: 0,
-		year: 2020,
-		make: 'Harley Davidson',
-		model: 'FXDB Street Box',
-	},
-	{
-		front: 0,
-		rear: 0,
-		year: 2018,
-		make: 'Harley Davidson',
-		model: 'FXEF Fat Bob',
-	},
 	{ front: 0, rear: 0, year: 2020, make: 'Yamaha', model: 'FZ1' },
 	{ front: 0, rear: 0, year: 2016, make: 'Yamaha', model: 'MT-09' },
 	{ front: 0, rear: 0, year: 2015, make: 'Yamaha', model: 'Virago XV400' },
--- screens/Bike.js
+++ screens/Bike.js
@@ -1,33 +1,97 @@
 //@ts-check
 
 import React, { useState, useEffect } from 'react';
-import PropTypes from 'prop-types';
+//import PropTypes from 'prop-types';
 import { StyleSheet, Text, View, Picker, Button } from 'react-native';
 import { TextInput } from 'react-native-gesture-handler';
+import RNPickerSelect from 'react-native-picker-select';
 
 import { Brands, Brands2 } from '../data/brands';
 
+const makePlaceholder = {
+	label: 'Provide your year...',
+	value: null,
+	color: '#9EA0A4',
+};
+
+const modelPlaceholder = {
+	label: 'Provide your make...',
+	value: null,
+	color: '#9EA0A4',
+};
+
 const Bike = () => {
-	const [brand, setBrand] = useState('');
-	const [model, setModel] = useState('');
+	const defaultSelectedModel = {
+		front: 0,
+		rear: 0,
+		year: 0,
+		make: '',
+		model: '',
+	};
+	const [year, setYear] = useState('');
+	const [makes, setMakes] = useState([{ label: '', value: '' }]);
+	const [make, setMake] = useState('');
 	const [models, setModels] = useState([]);
+	const [model, setModel] = useState('');
+	const [selectedModel, setSelectedModel] = useState(defaultSelectedModel);
 
-	/*
-const year = 2014;
-console.log(Brands2.filter(brand => brand.year === year));
-const brandsForYear = 
-  [...new Set(
-    Brands2.filter(brand => brand.year === year)
-    .map(b => b.make)
-  )];
+	const addMakeIfUnique = (arr, label) => {
+		const { length } = arr;
+		const id = length + 1;
+		const found = arr.some((el) => el.label === label);
+		if (!found) arr.push({ label, value: label });
+		return arr;
+	};
 
-console.log(brandsForYear.sort((a,b) => a<b ? -1 : a>b ? 1 : 0));
+	const getBrandsForYear = () => {
+		//TODO: implement GET http://localhost:5000/specs/makes/2014
 
-// for random travel numbers
-function getRndInteger(min, max) {
-  return Math.floor(Math.random() * (max - min) ) + min;
-}
-*/
+		const allMakesForYear = Brands2.filter(
+			(brand) => brand.year === parseInt(year),
+		).map((b) => Object.assign({ label: b.make, value: b.make }));
+
+		let brands = [];
+		allMakesForYear.forEach((brand) => {
+			addMakeIfUnique(brands, brand.label);
+		});
+
+		setMakes(brands);
+		setModels([]);
+		setModel('');
+		setSelectedModel(defaultSelectedModel);
+	};
+
+	useEffect(() => {
+		if (!make) return;
+
+		const modelsForYearAndMake = Brands2.filter(
+			(brand) => brand.year === parseInt(year) && brand.make === make,
+		).map((b) => Object.assign({ label: b.model, value: b.model }));
+
+		setModels(modelsForYearAndMake);
+		setModel('');
+		setSelectedModel(defaultSelectedModel);
+	}, [make]);
+
+	const getRndInteger = (min, max) => {
+		return Math.floor(Math.random() * (max - min + 1)) + min;
+	};
+
+	useEffect(() => {
+		if (!model) return;
+
+		let selectedModel = Brands2.filter(
+			(m) =>
+				m.year === parseInt(year) &&
+				m.make === make &&
+				m.model === model,
+		);
+		//TODO: remove once I have real travel numbers
+		selectedModel[0].front = getRndInteger(70, 230);
+		selectedModel[0].rear = getRndInteger(100, 230);
+
+		setSelectedModel(selectedModel[0]);
+	}, [model]);
 
 	return (
 		<View style={styles.container}>
@@ -38,46 +102,37 @@ function getRndInteger(min, max) {
 					style={styles.textInput}
 					maxLength={4}
 					keyboardType={'numeric'}
+					onChangeText={(value) => setYear(value)}
 				/>
-				<Button
-					onPress={() => console.log('load makes for ')}
-					title="Load Makes"
-				/>
+				<Button onPress={getBrandsForYear} title="Load Makes" />
 			</View>
 			<View style={styles.textInputContainer}>
-				<Text style={styles.textLabel}>Make:</Text>
-				<Picker
-					style={styles.twoPickers}
-					itemStyle={styles.twoPickerItems}
-					selectedValue={brand}
-					onValueChange={(brand, itemIndex) => setBrand(brand)}
-				>
-					{Brands.sort((a, b) => (a < b ? -1 : a > b ? 1 : 0)).map(
-						(brand) => (
-							<Picker.Item
-								key={brand}
-								label={brand}
-								value={brand}
-							/>
-						),
-					)}
-				</Picker>
+				<Text style={styles.pickerLabel}>Make:</Text>
+				<RNPickerSelect
+					style={pickerSelectStyles}
+					placeholder={makePlaceholder}
+					onValueChange={(selectedMake) => setMake(selectedMake)}
+					items={makes}
+				/>
 			</View>
-			<View style={styles.textInputContainer}>
-				<Text style={styles.textLabel}>Model:</Text>
-				<Picker
-					style={styles.twoPickers}
-					itemStyle={styles.twoPickerItems}
-					selectedValue={model}
-					onValueChange={(itemValue, itemIndex) =>
-						setModel(itemValue.toString())
-					}
-				>
-					{models.map((model) => (
-						<Picker.Item key={model} label={model} value={model} />
-					))}
-				</Picker>
+			<View style={[styles.textInputContainer, { marginTop: 10 }]}>
+				<Text style={styles.pickerLabel}>Model:</Text>
+				<RNPickerSelect
+					style={pickerSelectStyles}
+					placeholder={modelPlaceholder}
+					onValueChange={(selectedModel) => setModel(selectedModel)}
+					items={models}
+				/>
 			</View>
+			{selectedModel !== defaultSelectedModel && (
+				<View style={styles.container}>
+					<Text>
+						{`${selectedModel.year} ${selectedModel.model}`}
+					</Text>
+					<Text>Front travel: {selectedModel.front}</Text>
+					<Text>Rear travel: {selectedModel.rear}</Text>
+				</View>
+			)}
 		</View>
 	);
 };
@@ -98,7 +153,29 @@ Bike.displayName = 'Bike';
 
 export default Bike;
 
-// examples styles...modify or remove as needed
+const pickerSelectStyles = StyleSheet.create({
+	inputIOS: {
+		fontSize: 16,
+		paddingVertical: 12,
+		paddingHorizontal: 10,
+		borderWidth: 1,
+		borderColor: 'gray',
+		borderRadius: 4,
+		color: 'black',
+		paddingRight: 30, // to ensure the text is never behind the icon
+	},
+	inputAndroid: {
+		fontSize: 16,
+		paddingHorizontal: 10,
+		paddingVertical: 8,
+		borderWidth: 0.5,
+		borderColor: 'purple',
+		borderRadius: 8,
+		color: 'black',
+		paddingRight: 30, // to ensure the text is never behind the icon
+	},
+});
+
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
@@ -117,6 +194,10 @@ const styles = StyleSheet.create({
 		marginTop: 5,
 		marginRight: 5,
 	},
+	pickerLabel: {
+		marginTop: 15,
+		marginRight: 5,
+	},
 	textInput: {
 		borderColor: '#D6D7DA',
 		borderRadius: 3,
@@ -125,15 +206,5 @@ const styles = StyleSheet.create({
 		width: 50,
 		padding: 5,
 		fontSize: 12,
-	},
-	twoPickers: {
-		width: 200,
-		height: 68,
-		backgroundColor: 'aliceblue',
-		borderColor: 'black',
-		borderWidth: 1,
-	},
-	twoPickerItems: {
-		height: 68,
 	},
 });
